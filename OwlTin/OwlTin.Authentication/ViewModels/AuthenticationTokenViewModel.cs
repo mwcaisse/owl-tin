71f13dd134c7a07cd4709b370ec55be760871182
Mitchell Caisse <computers1055@gmail.com>
2018-09-27 21:59:17 -0400

    Added OwlTin authentication project

---
+++ OwlTin/OwlTin.Authentication/ViewModels/AuthenticationTokenViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mitchell.Authentication.ViewModels
+{
+    public class AuthenticationTokenViewModel
+    {
+        public string Username { get; set; }
+        public string AuthenticationToken { get; set; }
+    }
+}
